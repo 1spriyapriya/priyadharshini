237bd7d4c9cd3d26a553050e0ba660c2ba46984c
1spriyapriya <priyadharshinisekar001@gmail.com>
2018-05-29 12:32:46 +0530

    basuc

---
+++ HelloWorld1.java
@@ -0,0 +1,11 @@
+public class HelloWorld1
+{
+public static void main(String s[])
+{
+System.out.println("Welcome to java training");
+byte one=80;
+byte two=20;
+int three=one+two;
+System.out.println(three);
+}
+}
